a77dcd9574e34358f720465fbb6a5f4f90a9449d
swenginejsk <swenginejsk@gmail.com>
2017-11-24 07:46:32 +0900

    사용자
    약정 : 약정 선택/취소.변경
    설문지 조회
    설문 답변/문항답변/답변 내용 조회
    사용자 탈퇴/프로필 사진 업로드
    
    간단히 틀만 구축해놓은 상태임

---
+++ js/_survey.js
@@ -0,0 +1,16 @@
+/**
+ * @apiDefine adminError
+ * @apiError 401Error  AUTHORIZATION_REQUIRED
+ */
+
+/**
+ * @api                 {get}           /survey a.설문지 조회
+ * @apiVersion          0.1.0
+ * @apiName             survey select
+ * @apiGroup            Survey
+ *
+ * @apiDescription
+ * Create a new instance of the model and persist it into the data source
+ *
+ * @apiUse           adminError
+ */
