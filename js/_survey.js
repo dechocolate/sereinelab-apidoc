e68ed8a8982880ce00155a2e918e305f3a8349b9
june <kimjy4536@hotmail.com>
2018-01-16 17:27:13 +0900

    no message

--- js/_survey.js
+++ js/_survey.js
@@ -14,94 +14,42 @@
  * @apiParam          {ObjectId}        id             설문지 아이디  
  * @apiParam          {Json}            filter         filter 
  *  
- * 
+ *
+ * @apiSuccess          {ObjectId}      id                           	설문지 아이디  
  * @apiSuccess          {String}        title                           설문지 제목      
  * @apiSuccess          {String}        desc                            설문지 설명
- * @apiSuccess          {Integer}        surveyQuestion.type             타입(객관식 = 0 / 주관식 = 1 / 양자 = 2 / 범위 = 3 / 정도 = 4)
- * @apiSuccess          {String}        surveyQuestion.title             질문
- * @apiSuccess          {String}        surveyQuestion.mapping           맵핑
- * @apiSuccess          {Boolean}        surveyQuestion.display          사용 여부 (default : true)
- * @apiSuccess          {Integer}        surveyQuestion.max              최대값
- * @apiSuccess          {Integer}        surveyQuestion.min              최소값
- * @apiSuccess          {Object}         surveyQuestion.items             문항 Items
+ 
+ * @apiSuccess        	{ObjectId}        categories_id             	설문지 카테고리 아이디
+ * @apiSuccess        	{String}        categories_title             	설문지 카테고리 질문/내용
+ 
+ * @apiSuccess        	{ObjectId}        categories_questions_id       설문지 문항 아이디
+ * @apiSuccess        	{String}        categories_questions_title      설문지 문항 질문/내용
+ * @apiSuccess        	{String}        categories_questions_type       설문지 문항 타입(객관식=0 / 주관식=1 / 범위선택=2 / 양자택일=3)
+ 
+ * @apiSuccess          {Integer}        categories_questions_min       최소값 (문항 타입이 범위선택 인 경우)
+ * @apiSuccess          {Integer}        categories_questions_max       최대값 (문항 타입이 범위선택 인 경우)	
+ 
+ * @apiSuccess          {String}        categories_questions_items_text   설문지 문항 보기 내용
+ * @apiSuccess          {String}        categories_questions_items_vlaue  설문지 문항 보기 맵핑 값
+
  * 
- * @apiSuccessExample
- *  HTTP/1.1 200 OK
-   {
-    "title": "설문지 제목 1",
-    "desc": "설문지 설명 1",
-    "id": "5a1eaa6c31da970c3a31d269",
-    "surveyQuestions": [
-        {
-            "type": 0,
-            "title": "좋아하는 스포츠는?",
-            "mapping": "스포츠",
-            "display": true,
-            "items": [
-                {
-                    "text": "축구",
-                    "value": "0"
-                },
-                {
-                    "text": "야구",
-                    "value": "1"
-                },
-                {
-                    "text": "농구",
-                    "value": "2"
-                }
-            ],
-            "id": "5a307ed099377e35cc97d65b",
-            "surveyId": "5a1eaa6c31da970c3a31d269",
-            "max": null,
-            "min": null
-        },
-        {
-            "type": 1,
-            "title": "주관식 질문입니다.~~",
-            "mapping": "주관",
-            "display": true,
-            "items": null,
-            "id": "5a307ef499377e35cc97d65c",
-            "surveyId": "5a1eaa6c31da970c3a31d269",
-            "max": null,
-            "min": null
-        },
-        {
-            "type": 2,
-            "title": "부먹, 찍먹 ?",
-            "mapping": "탕수육",
-            "display": true,
-            "items": [
-                {
-                    "text": "부먹",
-                    "value": "0"
-                },
-                {
-                    "text": "찍먹",
-                    "value": "1"
-                }
-            ],
-            "id": "5a307f2899377e35cc97d65d",
-            "surveyId": "5a1eaa6c31da970c3a31d269",
-            "max": null,
-            "min": null
-        },
-        {
-            "type": 3,
-            "title": "좋아하는 숫자를 고르세요.",
-            "mapping": "숫자",
-            "display": true,
-            "items": null,
-            "id": "5a307f4399377e35cc97d65e",
-            "surveyId": "5a1eaa6c31da970c3a31d269",
-            "max": "10",
-            "min": "1"
-        }
-    ]
+ * @apiSuccessExample   {json}      Filter-example (상세조회 쿼리)
+{
+	"include": [
+		{
+			"relation": "categories",
+			"scope": {
+				"include": [
+					{
+						"relation": "questions",
+						"scope": {
+							"fields": ["id", "surveyQuestionCategoryId", "type", "title", "items"],
+							"where": {"display":true}		
+						}
+					}
+				]
+			}
+		}
+	]
 }
- *  @apiSuccessExample   {json}      Filter-example (상세조회 쿼리)
-    {
-		"include": "surveyQuestions" 		
-	} 	
  */
